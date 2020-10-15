d18dfb2af5c855dcfb42fb81eb542f83c601be3f
saipulanwarr <saipul0994@gmail.com>
2020-10-15 22:10:29 +0700

    added page register one

---
+++ app/src/main/java/com/example/myticket/RegisterOneAct.java
@@ -0,0 +1,28 @@
+package com.example.myticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class RegisterOneAct extends AppCompatActivity {
+
+    LinearLayout btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_one);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backtogetstarted = new Intent(RegisterOneAct.this, GetStartedAct.class);
+                startActivity(backtogetstarted);
+            }
+        });
+    }
+}
