c6543698db891e8f6f43412b48d53a47582ba44d
saipulanwarr <saipul0994@gmail.com>
2020-10-21 22:38:33 +0700

    added page edit profile

--- app/src/main/java/com/example/myticket/MyProfileAct.java
+++ app/src/main/java/com/example/myticket/MyProfileAct.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MyProfileAct extends AppCompatActivity {
 
     LinearLayout item_my_ticket;
+    Button btn_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class MyProfileAct extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
         item_my_ticket = findViewById(R.id.item_my_ticket);
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+
         item_my_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gotomytickets = new Intent(MyProfileAct.this, MyTicketDetailAct.class);
                 startActivity(gotomytickets);
+            }
+        });
+
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoeditprofile = new Intent(MyProfileAct.this, EditProfileAct.class);
+                startActivity(gotoeditprofile);
             }
         });
     }
