e2bffa984bf1be6e9b130ef36a2f25bd9d5ffbc9
saipulanwarr <saipul0994@gmail.com>
2020-10-22 13:59:09 +0700

    add animation to the get started screen

--- app/src/main/java/com/example/myticket/GetStartedAct.java
+++ app/src/main/java/com/example/myticket/GetStartedAct.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStartedAct extends AppCompatActivity {
 
     Button btn_sign_in, btn_new_account_create;
+    Animation ttb, btt;
+    ImageView emblem_app;
+    TextView intro_app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,18 @@ public class GetStartedAct extends AppCompatActivity {
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_new_account_create = findViewById(R.id.btn_new_account_create);
+
+        emblem_app = findViewById(R.id.emblem_app);
+        intro_app = findViewById(R.id.intro_app);
+
+        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        emblem_app.startAnimation(ttb);
+        intro_app.startAnimation(ttb);
+
+        btn_sign_in.startAnimation(btt);
+        btn_new_account_create.startAnimation(btt);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
