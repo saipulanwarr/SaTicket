56f1ee8955fbf2ccba5f92226c443a45bcdc19b0
saipulanwarr <saipul0994@gmail.com>
2020-10-22 13:45:45 +0700

    add animation to the splash screen

--- app/src/main/java/com/example/myticket/MainActivity.java
+++ app/src/main/java/com/example/myticket/MainActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation app_splash, btt;
+    ImageView app_logo;
+    TextView app_subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        app_logo = findViewById(R.id.app_logo);
+        app_subtitle = findViewById(R.id.app_subtitle);
+
+        app_logo.startAnimation(app_splash);
+        app_subtitle.startAnimation(btt);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -21,6 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(gogetstarted);
                 finish();
             }
-        }, 1000);
+        }, 2000);
     }
 }
