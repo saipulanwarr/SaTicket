720a03be7b1bce3499ce30f879c8823f68f0e235
saipulanwarr <saipul0994@gmail.com>
2020-10-24 03:05:52 +0700

    add animation on success buy

--- app/src/main/java/com/example/myticket/SuccessBuyTicketAct.java
+++ app/src/main/java/com/example/myticket/SuccessBuyTicketAct.java
@@ -3,12 +3,40 @@ package com.example.myticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuccessBuyTicketAct extends AppCompatActivity {
+
+    Animation app_splash, ttb, btt;
+    Button btn_view_ticket, btn_my_dashboard;
+    TextView app_title, app_subtitle;
+    ImageView icon_success_ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_buy_ticket);
+
+        btn_view_ticket = findViewById(R.id.btn_view_ticket);
+        btn_my_dashboard = findViewById(R.id.btn_my_dashboard);
+        app_title = findViewById(R.id.app_title);
+        app_subtitle = findViewById(R.id.app_subtitle);
+        icon_success_ticket = findViewById(R.id.icon_success_ticket);
+
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        icon_success_ticket.startAnimation(app_splash);
+
+        app_title.startAnimation(ttb);
+        app_subtitle.startAnimation(ttb);
+
+        btn_view_ticket.startAnimation(btt);
+        btn_my_dashboard.startAnimation(btt);
     }
 }
