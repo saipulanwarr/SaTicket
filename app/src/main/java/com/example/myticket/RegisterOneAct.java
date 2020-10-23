ece4fa9d7a5e77d2a1d4f2e5615f3ca4422f9c89
saipulanwarr <saipul0994@gmail.com>
2020-10-24 05:13:36 +0700

    save register data to firebase

--- app/src/main/java/com/example/myticket/RegisterOneAct.java
+++ app/src/main/java/com/example/myticket/RegisterOneAct.java
@@ -1,27 +1,66 @@
 package com.example.myticket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneAct extends AppCompatActivity {
 
     LinearLayout btn_back;
     Button btn_continue;
+    EditText username, password, email_address;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_one);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email_address = findViewById(R.id.email_address);
+
         btn_continue = findViewById(R.id.btn_continue);
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, username.getText().toString());
+                editor.apply();
+
+                reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        snapshot.getRef().child("username").setValue(username.getText().toString());
+                        snapshot.getRef().child("password").setValue(password.getText().toString());
+                        snapshot.getRef().child("email_address").setValue(email_address.getText().toString());
+                        snapshot.getRef().child("user_balance").setValue(800);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 Intent gotonextregister = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
                 startActivity(gotonextregister);
             }
