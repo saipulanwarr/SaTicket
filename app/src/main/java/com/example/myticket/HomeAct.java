e73d832f6772fe0472c12442b02ff67ca1968a1d
saipulanwarr <saipul0994@gmail.com>
2020-10-28 14:10:04 +0700

    home integration with firebase

--- app/src/main/java/com/example/myticket/HomeAct.java
+++ app/src/main/java/com/example/myticket/HomeAct.java
@@ -1,26 +1,67 @@
 package com.example.myticket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.github.florent37.shapeofview.shapes.CircleView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class HomeAct extends AppCompatActivity {
 
     LinearLayout btn_ticket_pisa;
     CircleView btn_to_profile;
+    ImageView photo_home_user;
+    TextView user_balance, nama_lengkap, bio;
+
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
+    String username_key_new = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        getUsernameLocal();
+
         btn_ticket_pisa = findViewById(R.id.btn_ticket_pisa);
         btn_to_profile = findViewById(R.id.btn_to_profile);
+        photo_home_user = findViewById(R.id.photo_home_user);
+        user_balance = findViewById(R.id.user_balance);
+        nama_lengkap = findViewById(R.id.nama_lengkap);
+        bio = findViewById(R.id.bio);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username_key_new);
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                nama_lengkap.setText(snapshot.child("nama_lengkap").getValue().toString());
+                user_balance.setText("US$ " + snapshot.child("user_balance").getValue().toString());
+                bio.setText(snapshot.child("bio").getValue().toString());
+                Picasso.with(HomeAct.this).load(snapshot.child("url_photo_profile").getValue().toString()).centerCrop().fit().into(photo_home_user);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         btn_ticket_pisa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +78,10 @@ public class HomeAct extends AppCompatActivity {
                 startActivity(gotoprofile);
             }
         });
+    }
+
+    public void getUsernameLocal(){
+        SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+        username_key_new = sharedPreferences.getString(username_key, "");
     }
 }
